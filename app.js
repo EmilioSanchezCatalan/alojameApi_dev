84f897d505c9e95d7d10a89d53429c9856fcc6d4
Emilio Sánchez <esc00019@red.ujaen.es>
2018-05-14 17:25:12 +0200

    renamed files and route for SAML auth

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/xxx', authRouter);
+app.use('/saml-auth', authRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
